1906028fd3b1e9068af9c82c2d728f3c83da5c95
tae yoon <tyoon@hotmail.com>
2014-07-10 18:15:53 -0400

    fixed wrong ip number

--- mpstd-web01-proxy.js
+++ mpstd-web01-proxy.js
@@ -9,8 +9,8 @@ var https = require('https'),
 //forward to web01's https proxy in the browser
 http.createServer(function (req, res) {
   res.writeHead(301, { 'Content-Type': 'text/plain' , 
-                       'Location'    : 'https://'+'198.135.15.19'+req.url});
-  												//198.135.15.93 for dev
+                       'Location'    : 'https://'+'198.135.14.61'+req.url});
+  												//198.135.15.93 for dev, 198.135.15.19 for test
      res.end('Redirecting to Web01\n');
   }).listen(8892);
 //
