227a5fc0b1c006725eca1cd3d7416a73266d27d4
tae yoon <tyoon@hotmail.com>
2014-07-11 15:44:52 -0400

    fault toloerant proxy with multiple servers

--- Digital-web01-proxy.js
+++ Digital-web01-proxy.js
@@ -1,51 +1,74 @@
 var https = require('https'),
     http  = require('http'),
-    //http2  = require('http'),
     util  = require('util'),
     path  = require('path'),
     fs    = require('fs'),
     fixturesDir = path.join(__dirname, 'test', 'fixtures'),
     httpProxy = require('./lib/http-proxy');
-//forward to web01's https proxy in the browser
- http.createServer(function (req, res) {
+    
+ //forward to web01's https proxy in the browser
+ var server1 =http.createServer(function (req, res) {
   //get rid of port number
   var index =req.headers.host.indexOf(":");
   var hostnameOnly = req.headers.host.substr(0,index);
-  console.log('Digital-web01-8892 hostname : ' +req.headers.host); 
-  console.log('Digital-web01-8892 url part: '+ req.url);
+  // console.log('Digital-web01-8892 hostname : ' +req.headers.host +"  "+hostnameOnly); 
+  // console.log('Digital-web01-8892 url part: '+ req.url);
   res.writeHead(301, { 'Content-Type': 'text/plain' , 
                        'Location'    : 'https://'+hostnameOnly+req.url});
   												//198.135.15.93 for dev, 198.135.15.19 for test
      res.end('Redirecting to Web01\n');
   }).listen(8892);
-
- http.createServer(function (req, res) {
-  //get rid of port number
-  var index =req.headers.host.indexOf(":");
-  var hostnameOnly = req.headers.host.substr(0,index);
-  console.log('Digital-web01-80 hostname : ' +req.headers.host); 
-  console.log('Digital-web01-80 url part: '+ req.url);
+ // web01 , SOA
+ var server2 = http.createServer(function (req, res) {
+ 
+  // console.log('Digital-web01-80 hostname : ' +req.headers.host ); 
+  // console.log('Digital-web01-80 url part: '+ req.url);
   //router
-  var soa_bool = hostnameOnly.match(/soa/i);
+  var soa_bool = req.headers.host.match(/soa/i);
   if (soa_bool){
     res.writeHead(301, { 'Content-Type': 'text/plain' , 
-                       'Location'    : 'https://'+hostnameOnly+':3001'+req.url});
+                       'Location'    : 'https://'+req.headers.host+':3001'+req.url});
                           //198.135.15.93 for dev, 198.135.15.19 for test
      res.end('Redirecting to Soa\n');
   }
   else{//default
     res.writeHead(301, { 'Content-Type': 'text/plain' , 
-                       'Location'    : 'https://'+hostnameOnly+req.url});
+                       'Location'    : 'https://'+req.headers.host+req.url});
                           //198.135.15.93 for dev, 198.135.15.19 for test
      res.end('Redirecting to Web01\n');
    }
 
   }).listen(80);
+  //SOA
+  var server3 =http.createServer(function (req, res) {
+  //get rid of port number
+  var index =req.headers.host.indexOf(":");
+  var hostnameOnly = req.headers.host.substr(0,index);
+  res.writeHead(301, { 'Content-Type': 'text/plain' , 
+                       'Location'    : 'https://'+hostnameOnly+':3001'+req.url}); //...18 for Test
   
+     res.end('Redirecting to SOA\n');
+  }).listen(8001);
+
+  server1.on('error', function (e) {
+  console.log('crash on port 8892');
+  // Handle your error here
+  console.log(e);
+  });
+  server2.on('error', function (e) {
+    console.log('crash on port 80');
+  // Handle your error here
+  console.log(e);
+  });
+  server3.on('error', function (e) {
+    console.log('crash on port 80');
+  // Handle your error here
+  console.log(e);
+  });
 //
 // proxy HTTPS to Web01 internal ip
 //
-httpProxy.createServer({
+var web01Proxy =httpProxy.createServer({
   target: {
     host: '192.168.0.3',
     port: 8892
@@ -58,5 +81,35 @@ httpProxy.createServer({
      passphrase: 'tae123456'
   }
 }).listen(443);
+//
+// Create the HTTPS proxy server listening on port 8002 to SOA server()
+//
+var soaProxy =httpProxy.createServer({
+  target: {
+    host: '192.168.0.6',
+    port: 8001
+  },
+  ssl: {
+    cert : fs.readFileSync(path.join(__dirname, './fakecerts/ca2/', 'server2.crt')),
+     key  : fs.readFileSync(path.join(__dirname, './fakecerts', 'server.key')),
+     ca   : fs.readFileSync(path.join(__dirname, './fakecerts/ca2/', 'ca2.crt')),
+     passphrase: 'tae123456'
+  }
+}).listen(3001);
+// Listen for the `error` event on `proxy`.
+web01Proxy.on('error', function (err, req, res) {
+  console.log("Proxy failed to connect to the Target URL=https://" + req.headers.host);
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Web01 went wrong. :-( \n' + "Proxy failed to connect to the Target URL=https://" + req.headers.host);
+});
+soaProxy.on('error', function (err, req, res) {
+  console.log("Proxy failed to connect to the Target URL=https://" + req.headers.host);
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('SOA went wrong. :-( \n' + "Proxy failed to connect to the Target URL=https://" + req.headers.host);
+});
 
 util.puts('http proxy server ' + 'started '+ 'on port ' + '80,8892 ->443 ');
