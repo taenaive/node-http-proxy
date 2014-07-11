6b2a9438eb3651054630c0177c3219995bae10b7
tae yoon <taenaive@gmail.com>
2014-07-10 21:35:12 -0400

    test

--- Digital-web01-proxy.js
+++ Digital-web01-proxy.js
@@ -7,11 +7,11 @@ var https = require('https'),
     httpProxy = require('./lib/http-proxy');
 //forward to web01's https proxy in the browser
 // http.createServer(function (req, res) {
-//   res.writeHead(301, { 'Content-Type': 'text/plain' , 
-//                        'Location'    : 'https://'+'198.135.14.61'+req.url});
-//   												//198.135.15.93 for dev, 198.135.15.19 for test
-//      res.end('Redirecting to Web01\n');
-//   }).listen(8892);
+  res.writeHead(301, { 'Content-Type': 'text/plain' , 
+                       'Location'    : 'https://'+'198.135.14.61'+req.url});
+  												//198.135.15.93 for dev, 198.135.15.19 for test
+     res.end('Redirecting to Web01\n');
+  }).listen(8892);
 //
 // proxy HTTPS to Web01 internal ip
 //
