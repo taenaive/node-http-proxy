fa59e78780bb653b8ac423653926848d3d63e849
tae yoon <tyoon@hotmail.com>
2014-07-11 13:52:49 -0400

    test

--- Digital-web01-proxy.js
+++ Digital-web01-proxy.js
@@ -11,7 +11,7 @@ var https = require('https'),
   //get rid of port number
   var index =req.headers.host.indexOf(":");
   var hostnameOnly = req.headers.host.substr(0,index);
-  console.log('Digital-web01-8892 hostname : ' +hostnameOnly); 
+  console.log('Digital-web01-8892 hostname : ' +req.headers.host); 
   console.log('Digital-web01-8892 url part: '+ req.url);
   res.writeHead(301, { 'Content-Type': 'text/plain' , 
                        'Location'    : 'https://'+hostnameOnly+req.url});
@@ -23,7 +23,7 @@ var https = require('https'),
   //get rid of port number
   var index =req.headers.host.indexOf(":");
   var hostnameOnly = req.headers.host.substr(0,index);
-  console.log('Digital-web01-80 hostname : ' +hostnameOnly); 
+  console.log('Digital-web01-80 hostname : ' +req.headers.host); 
   console.log('Digital-web01-80 url part: '+ req.url);
   //router
   var soa_bool = hostnameOnly.match(/soa/i);
