9da4a65dcb06b7752410a3c1b1cad05cf8a13fa4
tae yoon <tyoon@hotmail.com>
2014-07-28 18:21:57 -0400

    added dummy test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+
+exports.Test = {
+    "Core  testing": function (test) {
+        test.expect(1);
+
+            test.equal(true, true, 'shoud pass');//future test
+            test.done();
+      
+    }
+}
